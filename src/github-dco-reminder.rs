8fa7e061a24404e4d51a9d9762aabde23a35abef
Ji Chen <jaykchen@gmail.com>
2023-03-10 19:52:05 -0500

    reuse existing DCO checker logic octo._get

--- src/github-dco-reminder.rs
+++ src/github-dco-reminder.rs
@@ -1,9 +1,10 @@
 use github_flows::{
-    get_octo, listen_to_event, octocrab::models::pulls::PullRequestAction,
-    octocrab::models::repos::Commit, EventPayload,
+    get_octo, listen_to_event, octocrab::models::pulls::PullRequestAction, EventPayload,
 };
+use http_req::uri::Uri;
+use lazy_static::lazy_static;
+use regex::Regex;
 use serde_json::Value;
-use slack_flows::send_message_to_channel;
 use tokio::*;
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -11,9 +12,12 @@ pub async fn run() -> anyhow::Result<()> {
     let owner = "jaykchen";
     let repo = "vitesse-lite";
 
-    listen_to_event(owner, repo, vec!["push", "created"], |payload| {
-        handler(owner, repo, payload)
-    })
+    listen_to_event(
+        owner,
+        repo,
+        vec!["push", "created", "pull_request"],
+        |payload| handler(owner, repo, payload),
+    )
     .await;
 
     Ok(())
@@ -22,69 +26,79 @@ pub async fn run() -> anyhow::Result<()> {
 // push_url = https://github.com/jaykchen/vitesse-lite/commit/930900e8b66d7d97c9bbdc7d57a1260fe1851a96
 
 async fn handler(owner: &str, repo: &str, payload: EventPayload) {
+    lazy_static! {
+        static ref RE: Regex =
+            Regex::new(r#"Signed-off-by: \w+ <[\w._%+-]+@[\w.-]+\.\w{2,4}>"#).unwrap();
+    }
     let octo = get_octo(Some(String::from(owner)));
 
     match payload {
-        EventPayload::PushEvent(e) => {
-            let commits = e.commits;
+        EventPayload::PullRequestEvent(e) => {
+            let pull = e.pull_request;
+            let full_name = pull
+                .repo
+                .unwrap()
+                .full_name
+                .unwrap_or("no repo name found".to_string());
 
-            for commit in commits {
-                let user = commit.author.name;
-                let commit_sha = commit.sha;
-                let commit_message = commit.message;
-                let text = format!("sha: {commit_sha}, message: {commit_message}");
-                send_message_to_channel("ik8", "general", text);
-                let is_signed_off_by = commit_message.contains("Signed-off-by:");
+            let pull_number = pull.number;
 
-                if !is_signed_off_by {
-                    let body = format!("@{user} please DCO sign your commit");
+            let commits_url =
+                format!("https://api.github.com/repos/{full_name}/pulls/{pull_number}/commits");
 
-                    send_message_to_channel("ik8", "general", "inside commenting func".to_string());
-                    let _ = octo
-                        .commits(owner, repo)
-                        .create_comment(commit_sha, body)
-                        .send()
-                        .await;
-                }
-            }
-        }
+            // let uri = Uri::try_from(commits_url.as_str()).unwrap();
 
-        EventPayload::UnknownEvent(e) => {
-            let payload = e.to_string();
-            let val: Value = serde_json::from_str(&payload).unwrap();
+            let mut json: Vec<serde_json::Value> = Vec::new();
 
-            match val["commits"].as_array() {
-                None => (),
+            match octo._get(&commits_url, None::<&()>).await {
+                Err(_) => (),
 
-                Some(commits) => {
-                    for commit in commits {
-                        let mut user = "";
-                        let mut commit_sha = "";
-                        let mut commit_message = "";
-                        user = commit["author"]["name"].as_str().unwrap();
-                        commit_sha = commit["sha"].as_str().unwrap();
-                        commit_message = commit["message"].as_str().unwrap();
-                        let text = format!("sha: {commit_sha}, message: {commit_message}");
-                        send_message_to_channel("ik8", "general", text);
-                        let is_signed_off_by = commit_message.contains("Signed-off-by:");
+                Ok(response) => match response.text().await {
+                    Err(_) => (),
 
-                        if !is_signed_off_by {
-                            let body = format!("@{user} please DCO sign your commit");
-
-                            send_message_to_channel(
-                                "ik8",
-                                "general",
-                                "from unknowevent".to_string(),
-                            );
-                            let _ = octo
-                                .commits(owner, repo)
-                                .create_comment(commit_sha, body)
-                                .send()
-                                .await;
-                        }
+                    Ok(body) => {
+                        let text = String::from_utf8_lossy(body.as_bytes());
+                        json = serde_json::from_str(&text)
+                            .map_err(|e| {
+                                format!(
+                                    "response parse error {} || {} || {}",
+                                    e.to_string(),
+                                    text,
+                                    commits_url
+                                )
+                            })
+                            .unwrap();
                     }
-                }
-            }
+                },
+            };
+
+            let commits: Vec<&str> = json
+                .iter()
+                .filter_map(|j| j["commit"]["message"].as_str())
+                .collect();
+
+            let is_dco_ok = commits
+                .iter()
+                .map(|c| {
+                    let msg = c.lines().last().unwrap_or_default();
+                    RE.is_match(msg)
+                })
+                .all(std::convert::identity);
+
+            let creator = &pull.user.unwrap().login;
+            let at_creator = format!("@{} ", creator);
+
+            // let ob = outbound::modify_issue(pull.number);
+
+            let ok = "dco ok";
+            let no = "dco wrong";
+
+            let msg: &str = if is_dco_ok { ok } else { no };
+            let body = format!("{at_creator} {msg}");
+            let _ = octo
+                .issues(owner, repo)
+                .create_comment(pull_number, body)
+                .await;
         }
 
         _ => (),
