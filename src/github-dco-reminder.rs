5c150fda35d5406fe5e29d9310e35aa683fdb297
Ji Chen <jaykchen@gmail.com>
2023-03-11 16:27:34 -0500

    deserialized with RepoCommit more error catching

--- src/github-dco-reminder.rs
+++ src/github-dco-reminder.rs
@@ -1,4 +1,10 @@
-use github_flows::{get_octo, listen_to_event, EventPayload};
+use anyhow::Error;
+use github_flows::{
+    get_octo, listen_to_event,
+    octocrab::models::repos::{Commit, RepoCommit},
+    octocrab::{FromResponse, Octocrab},
+    EventPayload,
+};
 use http_req::uri::Uri;
 use lazy_static::lazy_static;
 use regex::Regex;
@@ -29,11 +35,13 @@ async fn handler(owner: &str, repo: &str, payload: EventPayload) {
 
     if let EventPayload::PullRequestEvent(e) = payload {
         let pull = e.pull_request;
-        let full_name = pull
-            .repo
-            .unwrap()
-            .full_name
-            .unwrap_or("no repo name found".to_string());
+        let mut full_name = "".to_string();
+
+        match pull.repo {
+            None => {return;},
+
+            Some(repo) => {full_name = repo.full_name.unwrap_or("no repo name found".to_string());},
+        };
 
         let pull_number = pull.number;
 
@@ -41,38 +49,38 @@ async fn handler(owner: &str, repo: &str, payload: EventPayload) {
             format!("https://api.github.com/repos/{full_name}/pulls/{pull_number}/commits");
         // "https://api.github.com/repos/jaykchen/vitesse-lite/pulls/22/commits"
         // let uri = Uri::try_from(commits_url.as_str()).unwrap();
+        let json = octocrab
+            ._get(commits_url, None::<&()>)
+            .await
+            .unwrap()
+            .json::<Vec<RepoCommit>>()
+            .await;
 
-        let mut json: Vec<serde_json::Value> = Vec::new();
+        let mut is_dco_ok = false;
 
-        match octocrab._get(&commits_url, None::<&()>).await {
-            Err(_) => (),
-
-            Ok(response) => match response.text().await {
+        'outer: {
+            match json {
                 Err(_) => (),
-
-                Ok(body) => {
-                    // let text = String::from_utf8_lossy(body.as_bytes());
-                    send_message_to_channel("ik8", "general", body.clone());
-                    json = serde_json::from_slice(body.as_bytes())
-                        .map_err(|_e| {})
-                        .unwrap();
+                Ok(repo_commits) => {
+                    for repo_commit in repo_commits {
+                        let msg = repo_commit.commit.message;
+                        if !RE.is_match(&msg) {
+                            break 'outer;
+                        }
+                    }
+                    is_dco_ok = true;
                 }
-            },
+            };
         };
 
-        let commits: Vec<&str> = json
-            .iter()
-            .filter_map(|j| j["commit"]["message"].as_str())
-            .collect();
-        send_message_to_channel("ik8", "general", commits.clone().join(";"));
-
-        let is_dco_ok = commits
-            .iter()
-            .map(|c| {
-                let msg = c.lines().last().unwrap_or_default();
-                RE.is_match(msg)
-            })
-            .all(std::convert::identity);
+        // if let Ok(commits) = get_commits(octocrab, commits_url).await {
+        //     let is_dco_ok = commits
+        //         .iter()
+        //         .map(|c| {
+        //             let msg = c.lines().last().unwrap_or_default();
+        //             RE.is_match(msg)
+        //         })
+        //         .all(std::convert::identity);
 
         let creator = &pull.user.unwrap().login;
 
@@ -82,5 +90,35 @@ async fn handler(owner: &str, repo: &str, payload: EventPayload) {
             .issues(owner, repo)
             .create_comment(pull_number, body)
             .await;
+        // }
     }
 }
+
+// async fn get_commits(octocrab: &Octocrab, commits_url: String) -> anyhow::Result<Vec<String>> {
+//     let json = octocrab
+//         ._get(&commits_url, None::<&()>)
+//         .await?
+//         .json::<Vec<CommitMessage>>()
+//         .await?;
+//     Ok(json.into_iter().map(|c| c.message).collect())
+// }
+
+// async fn get_commits(octocrab: &Octocrab, commits_url: String) -> anyhow::Result<Vec<String>> {
+//     let response = octocrab._get(&commits_url, None::<&()>).await?;
+//     let body = response.text().await?;
+//     let mut comments = Vec::<String>::new();
+
+//     match serde_json::from_str::<serde_json::Value>(&body) {
+//         Err(_e) => {
+//             return Err(_e.into());
+//         }
+//         Ok(json) => {
+//             for j in json.as_array() {
+//                 if let Some(commit) = j["commit"]["message"].as_str() {
+//                     comments.push(commit);
+//                 }
+//             }
+//             return Ok(comments);
+//         }
+//     }
+// }
